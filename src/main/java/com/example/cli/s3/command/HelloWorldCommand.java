c1ed70cd6f6a9a3822a8dada4991863aee3eeec8
TeamPapaya <34426841+edmundackah@users.noreply.github.com>
2024-05-06 19:41:44 +0100

    refactored codebase

--- src/main/java/com/example/cli/s3/command/HelloWorldCommand.java
+++ src/main/java/com/example/cli/s3/command/HelloWorldCommand.java
@@ -1,23 +1,11 @@
 package com.example.cli.s3.command;
 
-import com.example.cli.s3.Service.S3Service;
-import com.example.cli.s3.context.TenantContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
 @ShellComponent
 public class HelloWorldCommand {
-
-    private final S3Service s3Service;
-    private final TenantContext tenantContext;
-
-    @Autowired
-    public HelloWorldCommand(S3Service s3Service, TenantContext tenantContext) {
-        this.s3Service = s3Service;
-        this.tenantContext = tenantContext;
-    }
 
     @ShellMethod(key = "greet")
     public String greet(@ShellOption String name) {
